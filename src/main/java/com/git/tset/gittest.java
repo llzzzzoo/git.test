840f68c161ce691af749ba3152c75c1217c26019
lzo <lzo@ll.com>
2022-03-29 19:16:02 +0800

    sceond commit

--- src/main/java/com/git/tset/gittest.java
+++ src/main/java/com/git/tset/gittest.java
@@ -3,5 +3,6 @@ package com.git.tset;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!!");
     }
 }
